04e43d143bb3728a5ea4c208eeac8cb4ae1b7da4
deniz Ay <denizay@std.iyte.edu.tr>
2022-04-25 13:31:30 +0300

    added button component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
+import React, { useState } from "react";
+import Button from "./components/Button";
+import "./App.css";
 function App() {
-  return <div className="App"></div>;
+  const [openedEditor, setOpenedEditor] = useState("html");
+  const onTabClick = (editorName) => {
+    setOpenedEditor(editorName);
+  };
+  return (
+    <div className="App">
+      <p>Welcome to the editor!</p>
+      <div className="tab-button-container">
+        <Button
+          title="HTML"
+          onClick={() => {
+            onTabClick("html");
+          }}
+        />
+        <Button
+          title="CSS"
+          onClick={() => {
+            onTabClick("css");
+          }}
+        />
+        <Button
+          title="Javascript"
+          onClick={() => {
+            onTabClick("js");
+          }}
+        />
+      </div>
+      <div className="editor-container">
+        {openedEditor === "html" ? (
+          <p>The HTML editor is open</p>
+        ) : openedEditor === "css" ? (
+          <p>The CSS editor is open</p>
+        ) : (
+          <p>The Javascript editor is open</p>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Editor.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import "codemirror/lib/codemirror.css";
+import { Controlled as ControlledEditorComponent } from "react-codemirror2";
+const Editor = ({ language, value, setEditorState }) => {
+  <div className="editor-container"></div>;
+};
+
+export default Editor;
