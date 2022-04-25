e4f6d33b16b0154eb047bdabba373352bc726619
deniz Ay <denizay@std.iyte.edu.tr>
2022-04-25 13:59:06 +0300

    created editor component

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,8 +1,26 @@
 import React, { useState } from "react";
 import "codemirror/lib/codemirror.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
 import { Controlled as ControlledEditorComponent } from "react-codemirror2";
 const Editor = ({ language, value, setEditorState }) => {
-  <div className="editor-container"></div>;
+  const handleChange = (editor, data, value) => {
+    setEditorState(value);
+  };
+  <div className="editor-container">
+    <ControlledEditorComponent
+      onBeforeChange={handleChange}
+      value={value}
+      className="code-mirror-wrapper"
+      options={{
+        lineWrapping: true,
+        lint: true,
+        mode: language,
+        lineNumbers: true,
+      }}
+    />
+  </div>;
 };
 
 export default Editor;
